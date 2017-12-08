dd15b4e66722467935de5218ca2b75aaba677dd0
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2017-12-07 19:46:48 -0800

    Implement deploy key read/delete

--- netlify/resource_deploy_key.go
+++ netlify/resource_deploy_key.go
@@ -34,12 +34,29 @@ func resourceDeployKeyCreate(d *schema.ResourceData, metaRaw interface{}) error 
 	return nil
 }
 
-func resourceDeployKeyRead(d *schema.ResourceData, meta interface{}) error {
-	// There is currently no read endpoint
+func resourceDeployKeyRead(d *schema.ResourceData, metaRaw interface{}) error {
+	meta := metaRaw.(*Meta)
+	params := operations.NewGetDeployKeyParams()
+	params.KeyID = d.Id()
+	resp, err := meta.Netlify.Operations.GetDeployKey(params, meta.AuthInfo)
+	if err != nil {
+		// Deleted remotely
+		if v, ok := err.(*operations.GetDeployKeyDefault); ok && v.Code() == 404 {
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	d.Set("public_key", resp.Payload.PublicKey)
 	return nil
 }
 
-func resourceDeployKeyDelete(d *schema.ResourceData, meta interface{}) error {
-	// Currently no delete endpoint, so just remove from state.
-	return nil
+func resourceDeployKeyDelete(d *schema.ResourceData, metaRaw interface{}) error {
+	meta := metaRaw.(*Meta)
+	params := operations.NewDeleteDeployKeyParams()
+	params.KeyID = d.Id()
+	_, err := meta.Netlify.Operations.DeleteDeployKey(params, meta.AuthInfo)
+	return err
 }
--- netlify/resource_deploy_key_test.go
+++ netlify/resource_deploy_key_test.go
@@ -6,24 +6,57 @@ import (
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/netlify/open-api/go/models"
+	"github.com/netlify/open-api/go/plumbing/operations"
 )
 
 func TestAccDeployKey_basic(t *testing.T) {
+	var key models.DeployKey
+
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDeployKeyDestroy,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccDeployKeyConfig,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDeployKeyExists("netlify_deploy_key.test"),
+					testAccCheckDeployKeyExists("netlify_deploy_key.test", &key),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckDeployKeyExists(n string) resource.TestCheckFunc {
+func TestAccDeployKey_disappears(t *testing.T) {
+	var key models.DeployKey
+
+	destroy := func(*terraform.State) error {
+		meta := testAccProvider.Meta().(*Meta)
+		params := operations.NewDeleteDeployKeyParams()
+		params.KeyID = key.ID
+		_, err := meta.Netlify.Operations.DeleteDeployKey(params, meta.AuthInfo)
+		return err
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDeployKeyDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDeployKeyConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccCheckDeployKeyExists("netlify_deploy_key.test", &key),
+					destroy,
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccCheckDeployKeyExists(n string, key *models.DeployKey) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -34,8 +67,43 @@ func testAccCheckDeployKeyExists(n string) resource.TestCheckFunc {
 			return fmt.Errorf("No membership ID is set")
 		}
 
+		meta := testAccProvider.Meta().(*Meta)
+		params := operations.NewGetDeployKeyParams()
+		params.KeyID = rs.Primary.ID
+		resp, err := meta.Netlify.Operations.GetDeployKey(params, meta.AuthInfo)
+		if err != nil {
+			return err
+		}
+
+		*key = *resp.Payload
 		return nil
 	}
+}
+
+func testAccCheckDeployKeyDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "netlify_deploy_key" {
+			continue
+		}
+
+		meta := testAccProvider.Meta().(*Meta)
+		params := operations.NewGetDeployKeyParams()
+		params.KeyID = rs.Primary.ID
+		resp, err := meta.Netlify.Operations.GetDeployKey(params, meta.AuthInfo)
+		if err == nil && resp.Payload != nil {
+			return fmt.Errorf("Resource still exists: %s", rs.Primary.ID)
+		}
+
+		if err != nil {
+			if v, ok := err.(*operations.GetDeployKeyDefault); ok && v.Code() == 404 {
+				return nil
+			}
+		}
+
+		return err
+	}
+
+	return nil
 }
 
 var testAccDeployKeyConfig = `resource "netlify_deploy_key" "test" {}`
