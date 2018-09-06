cf00c57b73650b49eed83e9af97f973914acc540
cgriggs01 <cgriggs@hashicorp.com>
2018-09-06 13:53:51 -0700

    change import path: mitchellh-->terraform-providers

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/mitchellh/terraform-provider-netlify/netlify"
+	"github.com/terraform-providers/terraform-provider-netlify/netlify"
 )
 
 func main() {
